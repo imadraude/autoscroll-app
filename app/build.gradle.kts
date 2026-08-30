30b68e3644f896f120fcf7c1a02509be917f6680
imadraude <59265133+imadraude@users.noreply.github.com>
2026-08-30 23:38:21 +0300

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.imadraude.autoscroller"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.imadraude.autoscroller"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
