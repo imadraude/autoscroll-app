0b7584771bb27db08b71b125fc568d504f161e2b
imadraude <59265133+imadraude@users.noreply.github.com>
2026-08-31 01:39:27 +0300

    Persist updater state

--- app/src/main/java/com/imadraude/autoscroller/AppPreferences.java
+++ app/src/main/java/com/imadraude/autoscroller/AppPreferences.java
@@ -4,6 +4,8 @@ final class AppPreferences {
 
     static final String FILE_NAME = "autoscroller_settings";
     static final String KEY_SWIPE_SPEED = "swipe_speed";
+    static final String KEY_UPDATE_DOWNLOAD_ID = "update_download_id";
+    static final String KEY_UPDATE_BUILD = "update_build";
 
     private AppPreferences() {
     }
